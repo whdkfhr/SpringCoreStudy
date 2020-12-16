b2e8ce63754ae3ba83e9a841b259219fbebd4ac8
whdkfhr <whwoals18@naver.com>
2020-12-16 15:04:19 +0900

    스프링으로 전환, 'ApplicationContext' 사용

--- src/main/java/me/arok/core/MemberApp.java
+++ src/main/java/me/arok/core/MemberApp.java
@@ -4,12 +4,17 @@ import me.arok.core.member.Grade;
 import me.arok.core.member.Member;
 import me.arok.core.member.MemberService;
 import me.arok.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
