b3bdd5580f5877d6cc093ab368195699f178d183
whdkfhr <whwoals18@naver.com>
2020-12-20 14:23:04 +0900

    @Congifuration 과 싱글톤

---
+++ src/test/java/me/arok/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,39 @@
+package me.arok.core.singleton;
+
+import me.arok.core.AppConfig;
+import me.arok.core.member.MemberRepository;
+import me.arok.core.member.MemberService;
+import me.arok.core.member.MemberServiceImpl;
+import me.arok.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.annotation.Annotation;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberservice -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderservice -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
