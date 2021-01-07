8a9c99b31c3144a0140a6f4f030c1c980945a0a0
whdkfhr <whwoals18@naver.com>
2021-01-07 16:49:23 +0900

    request 스코프와 Provider

---
+++ src/main/java/me/arok/core/web/LogDemoService.java
@@ -0,0 +1,18 @@
+package me.arok.core.web;
+
+import lombok.RequiredArgsConstructor;
+import me.arok.core.common.MyLogger;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
+    }
+}
